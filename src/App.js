ac5e9f8540ce21cc941b318a672fef71a5ec00d1
Florian Müller <flointex@gmail.com>
2018-09-13 17:41:20 +0200

    Adjusting Stateless Components with Arrow Functions

--- src/App.js
+++ src/App.js
@@ -23,15 +23,26 @@ class App extends React.Component {
             `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_Key}`
         );
         const data = await api_call.json();
-        console.log(data);
-        this.setState({
-            temperature: data.main.temp,
-            city: data.name,
-            country: data.sys.country,
-            humidity: data.main.humidity,
-            description: data.weather[0].description,
-            error: ''
-        });
+        if (city && country) {
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ''
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: 'Please enter a City and a Country'
+            });
+        }
     };
 
     render() {
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-class Weather extends React.Component {
-    render() {
-        return <div />;
-    }
-}
+const Weather = props => (
+    <div>
+        {props.city &&
+            props.country && (
+                <p>
+                    Location: {props.temperature},{props.city}
+                </p>
+            )}
+        {props.temperature && <p>Temperature: {props.temperature}</p>}
+        {props.humidity && <p> Humidity: {props.humidity}</p>}
+        {props.description && <p>Conditions: {props.description}</p>}
+        {props.error && <p>{props.error}</p>}
+    </div>
+);
 
 export default Weather;
